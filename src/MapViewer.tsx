a42d08b94b5a39d867ae0844cbe856a3ff9ed9a5
DeveloperRyou <DeveloperRyou@gmail.com>
2024-06-14 15:33:26 +0900

    FIX: Map style 추가

--- src/MapViewer.tsx
+++ src/MapViewer.tsx
@@ -4,11 +4,11 @@ import MapView from "react-native-maps";
 interface MapViewerProps {}
 
 export default function MapViewer({}: MapViewerProps) {
-  return <MapView />;
+  return <MapView style={styles.map} />;
 }
 
 const styles = StyleSheet.create({
-  image: {
+  map: {
     width: "100%",
     height: "100%",
   },
