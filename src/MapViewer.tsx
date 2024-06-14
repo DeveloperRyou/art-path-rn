87ffac131d8c8ef93fb833d9e7513bee067bc008
DeveloperRyou <DeveloperRyou@gmail.com>
2024-06-14 16:15:08 +0900

    FIX: apikey 주입

--- src/MapViewer.tsx
+++ src/MapViewer.tsx
@@ -1,10 +1,19 @@
 import { StyleSheet } from "react-native";
 import MapView from "react-native-maps";
+import MapViewDirections from "react-native-maps-directions";
 
 interface MapViewerProps {}
 
 export default function MapViewer({}: MapViewerProps) {
-  return <MapView style={styles.map} />;
+  return (
+    <MapView style={styles.map}>
+      <MapViewDirections
+        origin={{ latitude: 37.3318456, longitude: -122.0296002 }}
+        destination={{ latitude: 37.771707, longitude: -122.4053769 }}
+        apikey={process.env.EXPO_PUBLIC_API_KEY as string}
+      />
+    </MapView>
+  );
 }
 
 const styles = StyleSheet.create({
