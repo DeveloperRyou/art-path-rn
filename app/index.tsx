e0be57a3c340877dd3384c9895902d83a984f5a3
DeveloperRyou <DeveloperRyou@gmail.com>
2024-06-17 13:53:23 +0900

    FEAT: install google-signin to ios

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,54 @@
-import { Text } from "react-native";
+import { useEffect, useState } from "react";
+import { Alert, Button, Text, View } from "react-native";
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  isErrorWithCode,
+  statusCodes,
+  User,
+} from "@react-native-google-signin/google-signin";
 
 export default function Page() {
-  return <Text>Home page</Text>;
+  const [userInfo, setUserInfo] = useState<User | null>(null);
+
+  GoogleSignin.configure({
+    webClientId: "<FROM DEVELOPER CONSOLE>", // client ID of type WEB for your server. Required to get the `idToken` on the user object, and for offline access.
+    scopes: ["https://www.googleapis.com/auth/drive.readonly"], // what API you want to access on behalf of the user, default is email and profile
+    offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+    hostedDomain: "", // specifies a hosted domain restriction
+    forceCodeForRefreshToken: true, // [Android] related to `serverAuthCode`, read the docs link below *.
+    accountName: "", // [Android] specifies an account name on the device that should be used
+    iosClientId: "<FROM DEVELOPER CONSOLE>", // [iOS] if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
+    googleServicePlistPath: "", // [iOS] if you renamed your GoogleService-Info file, new name here, e.g. GoogleService-Info-Staging
+    openIdRealm: "", // [iOS] The OpenID2 realm of the home web server. This allows Google to include the user's OpenID Identifier in the OpenID Connect ID token.
+    profileImageSize: 120, // [iOS] The desired height (and width) of the profile image. Defaults to 120px
+  });
+
+  // Google 로그인 처리하는 함수
+  const handleSignin = async () => {
+    try {
+      const userInfo = await GoogleSignin.signIn();
+      setUserInfo(userInfo);
+    } catch (error) {
+      if (isErrorWithCode(error)) {
+        switch (error.code) {
+          case statusCodes.SIGN_IN_CANCELLED:
+            break;
+          case statusCodes.IN_PROGRESS:
+            break;
+          case statusCodes.PLAY_SERVICES_NOT_AVAILABLE:
+            break;
+          default:
+        }
+        Alert.alert("Error", error.message);
+      }
+    }
+  };
+
+  return (
+    <View>
+      <Text>{JSON.stringify(userInfo, null, 2)}</Text>
+      <Button title="Login" onPress={handleSignin} />
+    </View>
+  );
 }
