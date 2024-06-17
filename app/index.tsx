c43bca314c00e863ff12aa023639b94486da5bd6
DeveloperRyou <DeveloperRyou@gmail.com>
2024-06-17 14:36:22 +0900

    FIX: google 로그인 기능 추가

--- app/index.tsx
+++ app/index.tsx
@@ -8,21 +8,15 @@ import {
   User,
 } from "@react-native-google-signin/google-signin";
 
+GoogleSignin.configure({
+  // forceCodeForRefreshToken: true, // [Android] related to `serverAuthCode`, read the docs link below *.
+  // accountName: "", // [Android] specifies an account name on the device that should be used
+  iosClientId: "1049554119899-1klm3haf9m5ljnb7l1sm9j9qb6g1deq1.apps.googleusercontent.com",
+});
+console.log("GoogleSignin", GoogleSignin);
+
 export default function Page() {
   const [userInfo, setUserInfo] = useState<User | null>(null);
-
-  GoogleSignin.configure({
-    webClientId: "<FROM DEVELOPER CONSOLE>", // client ID of type WEB for your server. Required to get the `idToken` on the user object, and for offline access.
-    scopes: ["https://www.googleapis.com/auth/drive.readonly"], // what API you want to access on behalf of the user, default is email and profile
-    offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
-    hostedDomain: "", // specifies a hosted domain restriction
-    forceCodeForRefreshToken: true, // [Android] related to `serverAuthCode`, read the docs link below *.
-    accountName: "", // [Android] specifies an account name on the device that should be used
-    iosClientId: "<FROM DEVELOPER CONSOLE>", // [iOS] if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
-    googleServicePlistPath: "", // [iOS] if you renamed your GoogleService-Info file, new name here, e.g. GoogleService-Info-Staging
-    openIdRealm: "", // [iOS] The OpenID2 realm of the home web server. This allows Google to include the user's OpenID Identifier in the OpenID Connect ID token.
-    profileImageSize: 120, // [iOS] The desired height (and width) of the profile image. Defaults to 120px
-  });
 
   // Google 로그인 처리하는 함수
   const handleSignin = async () => {
@@ -33,22 +27,25 @@ export default function Page() {
       if (isErrorWithCode(error)) {
         switch (error.code) {
           case statusCodes.SIGN_IN_CANCELLED:
+            Alert.alert("User cancelled the login flow");
             break;
           case statusCodes.IN_PROGRESS:
+            Alert.alert("Error encountered while signing in");
             break;
           case statusCodes.PLAY_SERVICES_NOT_AVAILABLE:
+            Alert.alert("Play services not available or outdated");
             break;
           default:
         }
-        Alert.alert("Error", error.message);
       }
     }
   };
 
   return (
     <View>
+      <GoogleSigninButton />
       <Text>{JSON.stringify(userInfo, null, 2)}</Text>
-      <Button title="Login" onPress={handleSignin} />
+      <Button title="Login" onPress={() => handleSignin()} />
     </View>
   );
 }
